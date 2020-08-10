4e9c403641a222d22c181b6dd966eab47a6f6c8f
Les Stroud <lstroud@ctic-inc.com>
2020-08-10 12:34:10 -0400

    move to environment variable based configuration instead of embedded yaml

--- lib/config.py
+++ lib/config.py
@@ -11,6 +11,8 @@ class ConsumerConfig:
     shared_access_policy: str
     consumer_group: str = '$default'
     buffer_size: int = 1
+    checkpoint_store_conn_str: str = None
+    checkpoint_store_container_name: str = None
 
 
 @dataclass
@@ -38,7 +40,10 @@ class Configuration:
                 fully_qualified_namespace=environ.get('GDI_NAMESPACE'),
                 shared_access_policy=environ.get('GDI_SHARED_ACCESS_POLICY'),
                 consumer_group=environ.get('GDI_CONSUMER_GROUP', '$default'),
-                buffer_size=int(environ.get('GDI_BUFFER_SIZE', 1))
+                buffer_size=int(environ.get('GDI_BUFFER_SIZE', 1)),
+                checkpoint_store_conn_str=environ.get('GDI_CHECKPOINT_STORE_CONNECTION'),
+                checkpoint_store_container_name=environ.get('GDI_CHECKPOINT_STORE_CONTAINER')
+
             ),
             database=DatabaseConfig(
                 host=environ.get('GDI_DB_HOST'),
