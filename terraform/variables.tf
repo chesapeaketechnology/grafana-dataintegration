4dc14f90bac2d26e4021d2a371314884de1aed2a
Les Stroud <lstroud@ctic-inc.com>
2020-08-13 14:56:38 -0400

    Added terraform files into project

---
+++ terraform/variables.tf
@@ -0,0 +1,90 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Azure resource group in which to deploy"
+}
+
+variable "virtual_network_name" {
+  type        = string
+  description = "Name of Azure virtual network"
+}
+
+variable "location" {
+  type        = string
+  description = "Region to provision resources in"
+}
+
+variable "system_name" {
+  type        = string
+  description = "Name to use for the data science cluster being created"
+}
+
+variable "environment" {
+  type        = string
+  description = "Current Environment to provision within"
+}
+
+variable "default_tags" {
+  type        = map(string)
+  description = "Collection of default tags to apply to all resources"
+}
+
+variable "subnet_cidrs" {
+  type        = list(string)
+  description = "The subnet CIDRs the Grafana server should live within."
+}
+
+variable "db_host" {
+  type        = string
+  description = "The database fully qualified host name"
+}
+variable "db_port" {
+  type        = string
+  description = "The database connection port"
+  default     = "5432"
+}
+variable "db_name" {
+  type        = string
+  description = "The database name for the database connection"
+}
+variable "db_user" {
+  type        = string
+  description = "The username for the database connection"
+}
+variable "db_password" {
+  type        = string
+  description = "The password for the database connection"
+}
+variable "db_schema" {
+  type        = string
+  description = "The database schema being used for storing data points."
+  default     = "public"
+}
+
+variable "topics"{
+  type        = set(string)
+  description = "List of eventhubs to create under this eventhubs space"
+}
+
+variable "eventhub_keys" {
+  type        = list(string)
+  description = "The access keys for eventhub; ordered the same as the topics."
+}
+variable "eventhub_namespace" {
+  type        = string
+  description = "The fully qualified eventhub namespace"
+}
+variable "eventhub_shared_access_policies" {
+  type        = list(string)
+  description = "A list of names of the eventhub shared access policy used to authenticate to eventhub; ordered the same as the topics."
+}
+
+variable "checkpoint_store_connection_str" {
+  type        = string
+  description = "[Optional] The azure blob container connection string is checkpoiint storage is being used."
+  default     = ""
+}
+variable "checkpoint_store_container" {
+  type        = string
+  description = "[Optional] The azure blob container to be used for eventhub checkpoint storage."
+  default     = ""
+}
