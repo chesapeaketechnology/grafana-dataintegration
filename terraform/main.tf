1ae22feeafacee4d6796e0340dca71da0929c40f
Les Stroud <lstroud@ctic-inc.com>
2020-09-08 14:10:51 -0400

    Lowering cpu and memory resources to fit in max sized azure container group (4 cpu, 16GB)

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,8 +82,8 @@ resource "azurerm_container_group" "gfi_container_group" {
     content {
       name = join("-", ["gfi", replace(container.value, "_", "-"), "consumer"])
       image = "chesapeaketechnology/grafana-dataintegration:0.2.5"
-      cpu = "0.5"
-      memory = "1"
+      cpu = "0.25"
+      memory = "1.1"
 
       ports {
         port     = (3000 + index(tolist(var.topics), container.key))
